d927fcec6f1daeeb942e73618cdbcb842ec6221e
Caroline Borg <cawibo@gmail.com>
2021-12-22 18:57:32 +0100

    add python solution for day 21 part 2

--- 21-dirac_dice/main2.py
+++ 21-dirac_dice/main2.py
@@ -1,3 +1,6 @@
+from functools import lru_cache
+from itertools import product
+
 # each roll of a die creates 3 universes
 # each turn entails three rolls of the die
 # as such, 3*3*3 universes are created.
@@ -12,9 +15,6 @@
 # 4 and 8 are present three times each
 # 5 and 7 are present six times each
 # and finally, 6 is present seven times
-
-
-from functools import lru_cache
 
 
 @lru_cache(maxsize=None)
@@ -39,159 +39,43 @@ def turn(
     elif player_two_score >= 21:  # player two wins
         return (0, 1)
 
-    # print("scores", player_one_score, player_two_score)
-
     if active_player == 1:
-        moved_three = move(player_one_pos, 3)
-        moved_four = move(player_one_pos, 4)
-        moved_five = move(player_one_pos, 5)
-        moved_six = move(player_one_pos, 6)
-        moved_seven = move(player_one_pos, 7)
-        moved_eight = move(player_one_pos, 8)
-        moved_nine = move(player_one_pos, 9)
-
-        active_player = 2
-
-        res_three = turn(
-            active_player,
-            moved_three,
-            player_one_score + moved_three,
-            player_two_pos,
-            player_two_score,
-        )
-        res_four = turn(
-            active_player,
-            moved_four,
-            player_one_score + moved_four,
-            player_two_pos,
-            player_two_score,
-        )
-        res_five = turn(
-            active_player,
-            moved_five,
-            player_one_score + moved_five,
-            player_two_pos,
-            player_two_score,
-        )
-        res_six = turn(
-            active_player,
-            moved_six,
-            player_one_score + moved_six,
-            player_two_pos,
-            player_two_score,
-        )
-        res_seven = turn(
-            active_player,
-            moved_seven,
-            player_one_score + moved_seven,
-            player_two_pos,
-            player_two_score,
-        )
-        res_eight = turn(
-            active_player,
-            moved_eight,
-            player_one_score + moved_eight,
-            player_two_pos,
-            player_two_score,
-        )
-        res_nine = turn(
-            active_player,
-            moved_nine,
-            player_one_score + moved_nine,
-            player_two_pos,
-            player_two_score,
-        )
+        wins = [0, 0]
+        for rolls in product(range(1, 4), range(1, 4), range(1, 4)):
+            new_pos = move(player_one_pos, sum(rolls))
+            result = turn(
+                2, new_pos, player_one_score + new_pos, player_two_pos, player_two_score
+            )
+            wins[0] += result[0]
+            wins[1] += result[1]
 
     elif active_player == 2:
-        moved_three = move(player_two_pos, 3)
-        moved_four = move(player_two_pos, 4)
-        moved_five = move(player_two_pos, 5)
-        moved_six = move(player_two_pos, 6)
-        moved_seven = move(player_two_pos, 7)
-        moved_eight = move(player_two_pos, 8)
-        moved_nine = move(player_two_pos, 9)
+        wins = [0, 0]
+        for rolls in product(range(1, 4), range(1, 4), range(1, 4)):
+            new_pos = move(player_two_pos, sum(rolls))
+            result = turn(
+                1,
+                player_one_pos,
+                player_one_score,
+                new_pos,
+                player_two_score + new_pos,
+            )
+            wins[0] += result[0]
+            wins[1] += result[1]
 
-        active_player = 1
-
-        res_three = turn(
-            active_player,
-            moved_three,
-            player_one_score,
-            player_two_pos,
-            player_two_score + moved_three,
-        )
-        res_four = turn(
-            active_player,
-            player_one_pos,
-            player_one_score,
-            moved_four,
-            player_two_score + moved_four,
-        )
-        res_five = turn(
-            active_player,
-            player_one_pos,
-            player_one_score,
-            moved_five,
-            player_two_score + moved_five,
-        )
-        res_six = turn(
-            active_player,
-            player_one_pos,
-            player_one_score,
-            moved_six,
-            player_two_score + moved_six,
-        )
-        res_seven = turn(
-            active_player,
-            player_one_pos,
-            player_one_score,
-            moved_seven,
-            player_two_score + moved_seven,
-        )
-        res_eight = turn(
-            active_player,
-            player_one_pos,
-            player_one_score,
-            moved_eight,
-            player_two_score + moved_eight,
-        )
-        res_nine = turn(
-            active_player,
-            player_one_pos,
-            player_one_score,
-            moved_nine,
-            player_two_score + moved_nine,
-        )
-
-    player_one_wins = (
-        res_three[0]
-        + res_nine[0]
-        + res_four[0] * 3
-        + res_eight[0] * 3
-        + res_five[0] * 6
-        + res_seven[0] * 6
-        + res_six[0] * 7
-    )
-    player_two_wins = (
-        res_three[1]
-        + res_nine[1]
-        + res_four[1] * 3
-        + res_eight[1] * 3
-        + res_five[1] * 6
-        + res_seven[1] * 6
-        + res_six[1] * 7
-    )
-
-    return (player_one_wins, player_two_wins)
+    return wins
 
 
 assert move(7, 5) == 2
 assert move(10, 5) == 5
 
-res = turn(1, 4, 0, 8, 0)
-print(res)
+res = turn(1, 4, 0, 1, 0)
+print("part2:", max(res))
 
+# 437424806752258
+# 469604932270189
 # 341960390180808
 # 444356092776315
 # 40746749749147287
 # 55199972140670812
+# 93916751312613
